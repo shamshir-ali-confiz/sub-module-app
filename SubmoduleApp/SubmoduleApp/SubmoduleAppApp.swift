9f3c36b78ad5af25facedda841ba1dd5bfa2176f
Shamshir Ali <shamshir.ali@confiz.com>
2024-01-01 22:25:05 +0500

    init commit

---
+++ SubmoduleApp/SubmoduleApp/SubmoduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmoduleAppApp.swift
+//  SubmoduleApp
+//
+//  Created by Shamshir Ali on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SubmoduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SubmoduleApp/SubmoduleApp/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  SubmoduleApp
+//
+//  Created by Shamshir Ali on 01/01/2024.
+//
+
+import Foundation
+class Test {
+    func firstTest() -> String {
+        return "First Test"
+    }
+}
